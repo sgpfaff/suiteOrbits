9ece95293efad7f2033214839f782c7c17405e2b
Gabriel <gabriel.pfaffman@gmail.com>
2025-03-09 14:50:33 -0400

    Add functionality to Automatically run suite.

--- suiteorbits/suite/suite.py
+++ suiteorbits/suite/suite.py
@@ -125,7 +125,16 @@ class Suite():
     '''
     Suite class for exploring n parameters.
     '''
-    def __init__(self, dims, dim_ranges, dim_steps, potential, init_kwargs={}, **kwargs):
+    def __init__(self, 
+                 dims, 
+                 dim_ranges, 
+                 dim_steps, 
+                 potential, 
+                 integ_time={'t_start':0, 't_end':10, 'steps':10000}, 
+                 integ_kwargs={},
+                 init_kwargs={}, 
+                 show_updates = True,
+                 **kwargs):
         '''
         Parameters:
         ----------
@@ -140,6 +149,12 @@ class Suite():
 
         potential : galpy.potential.Potential
             The galpy potential object to use for the suite.
+
+        integ_time : dict
+            Dictionary with the start, end, and number of timesteps for the integration.
+
+        integ_kwargs : dict
+            Keyword arguments to pass to the integration function.
 
         init_kwargs : dict
             Additional keyword arguments to pass to the initializer function.
@@ -168,15 +183,41 @@ class Suite():
         self.potential = potential
         self.dim_ranges = dim_ranges
         self.dim_res = dim_steps
+        self.integ_time = integ_time
+        self.integ_kwargs = integ_kwargs
         self.init_kwargs = init_kwargs
+        self.ts = np.linspace(integ_time['t_start'], integ_time['t_end'], integ_time['steps']) # integration timesteps
         self.kwargs = kwargs
 
-        self.orbits = self._initialize()
+        if show_updates == True:
+            print(f'Initializing orbits...')
+        self.orbits = self._initialize() # intial conditions of the orbit, galpy orbit object
+        
+        if show_updates == True:
+            print(f'Integrating orbits...')
+        self._run()
         
     def _initialize(self):
         '''
         Initialize orbits
+
+        Returns
+        -------
+        galpy.orbit.Orbit
+            Orbit object with initial phase space positions of each orbit
         '''
         if self.dims == 'E' and 'Lz' in self.kwargs:
             # ADD: warn user if the potential is not spherically symmetric.
             return varyE_fixLz(self.dim_ranges, self.dim_res, self.kwargs['Lz'], self.potential, **self.init_kwargs)
+        
+    def _run(self):
+        '''
+        Run the suite
+
+        Returns
+        -------
+        None
+            Updates orbit internally
+        '''
+        self.orbits.integrate(self.ts, self.potential, **self.integ_kwargs)
+        return None
